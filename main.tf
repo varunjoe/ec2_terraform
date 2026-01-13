16c96b4288ea87d59f3c333f376ab04fcd8d5755
varunjoe <109220491+varunjoe@users.noreply.github.com>
2026-01-13 19:36:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,40 +20,42 @@ resource "aws_security_group" "all_traffic_sg" {
   }
 }
 
-data "aws_ami" "amazon_linux" {
+data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["ami-0ecb62995f68bb549"]
   }
 
   owners = ["amazon"]
 }
 
 resource "aws_instance" "docker_ec2" {
-  ami                    = data.aws_ami.amazon_linux.id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.all_traffic_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
+              apt-get update -y
 
               # Install Docker
-              yum install docker -y
+              apt-get install -y docker.io
               systemctl start docker
               systemctl enable docker
               usermod -aG docker ec2-user
 
               # Install Terraform
-              yum install -y yum-utils
-              yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-              yum install terraform -y
+              apt-get update && apt-get install -y gnupg software-properties-common
+              wget -O- apt.releases.hashicorp.com | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+              echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+              apt-get update && apt-get install terraform -y
 
               # Install AWS CLI v2
-              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              apt-get install unzip -y
+              curl "awscli.amazonaws.com" -o "awscliv2.zip"
               unzip awscliv2.zip
               ./aws/install
 
