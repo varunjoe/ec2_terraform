c902d969fd81c817ff023052c45604b40f1bfd72
varunjoe <109220491+varunjoe@users.noreply.github.com>
2026-01-14 11:13:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "919466768284-terraform-states"
+    key            = "ec2-project/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock" # Must be pre-created
+    encrypt        = true
+  }
+}
 
 provider "aws" {
   region = var.aws_region
 }
+
 
